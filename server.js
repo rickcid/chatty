2d1e471d3565af8bf4d612197fd324d84ecf4891
Rick Cid <rick@hispand.com>
2015-05-01 23:32:59 -0600

    Completed node server implementation, and modified with express

--- server.js
+++ server.js
@@ -1,43 +1,94 @@
-var messages = [{message:'hello'}];
 
-http = require('http');
-http.createServer(function(req, res) {
-  if (req.method === 'GET') {
-    res.writeHead(200, {
-      'Connection': 'close',
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
-    });
-    res.end(JSON.stringify(messages));
-  }
-  if (req.method === 'POST') {
-    var postData = '';
-    req.on('data', function(chunk) {
-      postData += chunk.toString();
-    });
-    req.on('end', function() {
-      console.log(JSON.parse(postData));
-      var msg = JSON.parse(postData);
-      messages.push(msg);
-      res.writeHead(200, {
-        'Connection': 'close',
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-      });
-      res.end(JSON.stringify(messages));
-    });
-  }
-  if (req.method === 'OPTIONS') {
-    res.writeHead(200, {
-        'Connection': 'close',
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
-        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
-      });
-    res.end("{}");
-  }
-}).listen(9000);
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser')
+
+var messages = [];
+
+app.use(bodyParser());
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+})
+
+app.get('/', function(req, res) {
+  res.send(JSON.stringify(messages));
+});
+
+app.post('/', function(req, res) {
+  console.log(req.body);
+  messages.push(req.body);
+  res.send(JSON.stringify(messages))
+})
+
+
+app.listen(9000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var messages = [{message:'hello'}];
+
+// http = require('http');
+// http.createServer(function(req, res) {
+//   if (req.method === 'GET') {
+//     res.writeHead(200, {
+//       'Connection': 'close',
+//       'Content-Type': 'application/json',
+//       'Access-Control-Allow-Origin': '*'
+//     });
+//     res.end(JSON.stringify(messages));
+//   }
+//   if (req.method === 'POST') {
+//     var postData = '';
+//     req.on('data', function(chunk) {
+//       postData += chunk.toString();
+//     });
+//     req.on('end', function() {
+//       console.log(JSON.parse(postData));
+//       var msg = JSON.parse(postData);
+//       messages.push(msg);
+//       res.writeHead(200, {
+//         'Connection': 'close',
+//         'Content-Type': 'application/json',
+//         'Access-Control-Allow-Origin': '*'
+//       });
+//       res.end(JSON.stringify(messages));
+//     });
+//   }
+//   if (req.method === 'OPTIONS') {
+//     res.writeHead(200, {
+//         'Connection': 'close',
+//         'Content-Type': 'application/json',
+//         'Access-Control-Allow-Origin': '*',
+//         'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+//         'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+//       });
+//     res.end("{}");
+//   }
+// }).listen(9000);
+
+
+///MyCode Here
+
 
 // var http = require("http");
 
